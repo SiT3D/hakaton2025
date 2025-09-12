21a4535c9cfa160502e39a7ad5701ec439323126
Alexander Zaitsev <sit3d3@gmail.com>
2025-09-12 23:29:54 +0300

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './HelloWorld.vue'
+</script>
+
+<template>
+  <h1>Hello Vue!</h1>
+
+  <HelloWorld name="HI!" />
+</template>
---
+++ frontend/src/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Hello, {{ name }}!</h2>
+  </div>
+</template>
+
+<script setup>
+
+defineProps({
+  name: String
+})
+</script>
